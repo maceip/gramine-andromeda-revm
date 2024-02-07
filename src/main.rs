edac5b1c9e621d36b362395e940c31fd32b30638
rpm <ryan.macarthur@gmail.com>
2024-02-07 10:47:28 -0400

    scaffold

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use clap::Parser;
 use std::io;
+use eyre::{eyre, Result};
+use structopt::StructOpt;
+use tracing::debug;
+
+use notary_server::{
+    init_tracing, parse_config_file, run_server, CliFields, NotaryServerError,
+    NotaryServerProperties,
+};
 
 use suave_andromeda_revm::StatefulExecutor;
 
@@ -13,15 +21,21 @@ struct Cli {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), NotaryServerError> {
     let cli_args = Cli::parse();
     let mut service = StatefulExecutor::new_with_rpc(cli_args.rpc.clone());
+    let cli_fields: CliFields = CliFields::from_args();
+    let config: NotaryServerProperties = parse_config_file(&cli_fields.config_file)?;
+
+    // Set up tracing for logging
+    init_tracing(&config).map_err(|err| eyre!("Failed to set up tracing: {err}"))?;
+
+    debug!(?config, "Server config loaded");
 
     // TODO: probably doesnt work due to async
     loop {
         let mut input_buf = String::new();
         io::stdin().read_line(&mut input_buf).expect("");
-
         match service
             .execute_command(input_buf.trim(), cli_args.trace)
             .await
